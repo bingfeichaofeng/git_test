f34516208e9e10666d937b7523703703508965bf
HWY <337192903@qq.com>
2022-05-07 10:33:25 +0800

    first commit

---
+++ src/main/java/com/bilibili/git/TestApplication.java
@@ -0,0 +1,14 @@
+package com.bilibili.git;
+
+/**
+ * @ClassName TestApplication
+ * @Author: HWY
+ * @Version: v1.0.0
+ * @Description: TODO
+ * @Date: 2022/5/7 10:26
+ */
+public class TestApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
